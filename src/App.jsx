f0f4a874797c71b1162ab926a96e9e6c4cd44da1
Chandrika.Ajmera <chandrika.ajmera@ness.com>
2025-05-20 15:05:02 +0530

    NetFlixGPT project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="bg-amber-300 text-3xl font-bold underline">
+        hello How are you!! Khana Khake jaaana haan!
+      </div>
+      
+      
+      
+    </>
+  )
+}
+
+export default App
